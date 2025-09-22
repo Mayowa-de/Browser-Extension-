5dafc284f200f4a7d6ca0175d35ed02969737f3f
Mayowa-de <mayortech03@gmail.com>
2025-09-22 01:38:25 -0700

    adjust the container shadow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import Logo from '../assets/images/logo.svg'
 export default function Hero() {
   return (
     <div className=''>
-      <div className='flex relative mt-4 gap-4 items-center w-full bg-white p-2 h-16 shadow-xl'>
+      <div className='flex mt-4 gap-4 items-center w-full bg-white p-2 h-16 shadow-md'>
         <img src={Logo} alt="Logo" className=' w-40 '/>
         <button className=' w-full  flex justify-end pr-2'>light</button>
       </div>
