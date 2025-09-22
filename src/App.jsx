a17bca9ff07844f0a230a145c55dbf011b4756c3
Mayowa-de <mayortech03@gmail.com>
2025-09-22 02:18:06 -0700

    center the title and Button components

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
   return (
       <div className='bg-[hsl(227, 75%, 14%)] gap-5 p-2 flex flex-col'>
         <Hero/>
+        <div className='flex flex-col gap-3 justify-center items-center'>
         <Title/>
         <Button/>
+        </div>
       </div>
   )
 }
