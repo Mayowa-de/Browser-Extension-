9c0339f91a087da9ab0f133aa0538b0d286c1e69
Mayowa-de <mayortech03@gmail.com>
2025-09-21 17:32:53 -0700

    add files

---
+++ src/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Date from './data.json'
+export default function Hero() {
+  return (
+    <div className='bg-green-600'>
+      <h3>hello world</h3>
+    </div>
+  )
+}
