3604b1f5f899f117a7cb17b9315251bcfb1b82a1
Mayowa-de <mayortech03@gmail.com>
2025-09-22 01:00:05 -0700

    render component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Hero from "./components/Hero"
+import Title from './components/Title'
 
 function App() {
 
   return (
       <div className='bg-[hsl(227, 75%, 14%)] p-2'>
         <Hero/>
+        <Title/>
       </div>
   )
 }
