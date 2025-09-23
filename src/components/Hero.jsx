d848ddb4249e6817ebeecf7c83b2321d8012f958
Mayowa-de <mayortech03@gmail.com>
2025-09-23 14:22:50 -0700

    adjust the import image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react'
 import './Hero.css'
-import Date from '../data.json'
+import Date from '../data'
 
 export default function Hero() {
   const [isToggle, setIsToggle] = useState(()=>{
@@ -21,13 +21,13 @@ export default function Hero() {
   },[isToggle])
   return (
       <div className='flex mt-4 gap-4 dark:bg-[hsl(226,25%,17%)] items-center rounded-xl w-full bg-white p-2 h-16 shadow-md'>
-        <img src='/public/images/logo.svg' alt="Logo" className=' w-40 '/>
+        <img src='/images/logo.svg' alt="Logo" className=' w-40 '/>
         <div className='w-full flex justify-end'>
         <button onClick={()=>setIsToggle(!isToggle)} className={` w-12 h-12 flex justify-center items-center rounded-xl ${!isToggle ? 'bg-[hsl(226,11%,37%)]' : 'bg-[linear-gradient(180deg,_#EBF2FC_0%,_#EEF8F9_100%)]'}`}>
         {isToggle ? (
-          <img src='./assets/images/icon-moon.svg' alt="moon icon" className='w-5 h-5 dark:text-white' />
+          <img src='/images/icon-moon.svg' alt="moon icon" className='w-5 h-5 dark:text-white' />
         ) : (
-          <img src='/public/images/icon-sun.svg' alt="sun icon" className='w-5 h-5 dark:text-white' />
+          <img src='/images/icon-sun.svg' alt="sun icon" className='w-5 h-5 dark:text-white' />
         )}
           </button>
       </div>
